b777f5104c7df0a46ef47afb1be26ebc0298687a
Shaid1998 <shaidur35-2300@diu.edu.bd>
2023-08-07 00:51:26 +0600

    Working on Header Part

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div>
+                <h1 class='text-center bold 100px'>Home</h1>
+            </div>
+
+        )
+    }
+}
